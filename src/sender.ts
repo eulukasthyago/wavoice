01668d8a7faca7c4e809f6d55db0a36d2ddecc69
Lucas Tiago <lucas.tiago@auaha.com.br>
2022-11-23 17:56:01 -0300

    Update

--- src/sender.ts
+++ src/sender.ts
@@ -1,25 +1,31 @@
 import { create, Whatsapp, Message, SocketState } from 'venom-bot';
 
 class Sender {
-	// private client: Whatsapp;
+	private client: Whatsapp | undefined;
 
 	constructor() {
 		this.initialize();
 	}
 
+	async sendText(to: string, body: string) {
+		this.client?.sendText(to, body);
+	}
+
 	private initialize() {
-		const qr = (base64Qrimg: string) => {
+		const qr = (base64QrImg: string) => {
 			console.log();
 		}
 		const statusBeluga = (statusSession: string, session:string) => {
-			return statusSession;
+			console.log(statusSession);
 		}
 
 		const start = (client: Whatsapp) => {
-			// this.client = client;
+			this.client = client;
+			this.sendText('5514991254954@c.us', "Olá tudo bem, esse é um teste");
+			console.log(client);
 		}
 
-		create('statusBeluga', qr, statusBeluga)
+		create('wavoice', qr, undefined,)
 		.then((client) => start(client))
 		.catch((error) => console.error(error));
 	}
