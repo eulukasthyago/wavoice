5903b0e00407cc311dc8c9da43fafd97a0ba867b
Lucas Tiago <lucas.tiago@auaha.com.br>
2022-11-24 13:34:50 -0300

    Melhorias

--- src/sender.ts
+++ src/sender.ts
@@ -1,40 +1,50 @@
 import { create, Whatsapp, Message, SocketState } from 'venom-bot';
-
+import audioTetse from './assets/audio.json';
 class Sender {
-	private client: Whatsapp | undefined;
+  private client: Whatsapp | undefined;
 
-	constructor() {
-		this.initialize();
-	}
+  constructor() {
+    this.initialize();
+  }
 
-	async sendText(to: string, body: string) {
-			await this.client?.sendVoice(to, body).then((result: any) => {
-				console.log('Foi', result);
-			}).catch((error: string) => {
-				console.log('Deu erro', error);
-			});
-	}
+  async sendText(to: string, body: string) {
+      await this.client?.sendText(to, body);
+  }
 
-	private initialize() {
-		const qr = (base64QrImg: string) => {
-			console.log();
-		}
-		const statusBeluga = (statusSession: string, session:string) => {
-			console.log(statusSession);
-		}
+  async sendAudio(to: string, body: string){
+    await this.client?.sendVoiceBase64(to, body).then((result: any) => {
+      console.log('Foi', result);
+    }).catch((error: string) => {
+      console.log('Deu erro', error);
+    });
+  }
 
-		const start = (client: Whatsapp) => {
-			this.client = client;
-			this.sendText('5514991254954@c.us', "/home/eulukasthyago/Documentos/projetos/Node/wavoice/assets/audio.mp3");
-		}
+  private initialize() {
+    const qr = (base64QrImg: string) => {
+      console.log();
+    }
+    const statusBeluga = (statusSession: string, session:string) => {
+      console.log(statusSession);
+    }
 
-		create('wavoice', qr, undefined)
-		.then((client) => {
-			start(client);
+    const start = (client: Whatsapp) => {
+      this.client = client;
+      this.sendText('5514991254954@c.us', "Estou fazendo um teste");
+      this.sendAudio('5514991254954@c.us', audioTetse.mp3);
+    }
+
+    create('wavoice-teste', qr, undefined, {
+      // debug: true,
+      // devtools: true,
+      // headless: false,
+      // useChrome: true
+    })
+    .then((client) => {
+      start(client);
 // 			console.log("Teste", client);
-		})
-		.catch((error) => console.error(error));
-	}
+    })
+    .catch((error) => console.error(error));
+  }
 }
 
 export default Sender;
